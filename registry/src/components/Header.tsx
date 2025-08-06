058f92e8c296f06f18c633156b89acb12171ccc0
DebaKaran <karan.deba@gmail.com>
2025-08-06 13:56:56 +0530

    Add SearchInput component with navigation on submit
    - Implemented controlled input using useState for search term
    - On form submission, prevent default and navigate to /search?term=...
    - Integrated useNavigate from React Router for programmatic routing

--- registry/src/components/Header.tsx
+++ registry/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
 
 const Header = () => {
   return (
     <>
       <Link to="/">NPM Registry</Link>
+      <SearchInput />
     </>
   );
 };
